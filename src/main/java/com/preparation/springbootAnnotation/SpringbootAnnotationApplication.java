e8bc54d422e8ef0c90909d9e8c0bb2cdceb00a59
Pradnya Thorat <pradnya.anil.thorat@accenture.com>
2024-07-10 19:46:20 +0530

    initial commit

---
+++ src/main/java/com/preparation/springbootAnnotation/SpringbootAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.preparation.springbootAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAnnotationApplication.class, args);
+	}
+
+}
